81fa6763cc0dff34ab8c287eb5c163cd0ecf7069
Thor Muller <thor@thormuller.com>
2014-07-16 14:36:41 -0700

    Removed those pesky smart quotes

--- yxyy.js
+++ yxyy.js
@@ -1,6 +1,6 @@
 
 var yaysayers = require(‘data/yaysayers.json’); // all the people
-var modes = [‘make', ‘connect’, ‘hack', ‘swim', ‘dance', ‘think', ‘reflect’, ’sing’, ‘imagine’, ‘love’, ‘celebrate’, ‘thank’]; // for starters
+var modes = ['make', 'connect', 'hack', 'swim', 'dance', 'think', 'reflect', 'sing', 'imagine', 'love', 'celebrate', 'thank']; // for starters
 var seed =  require(‘libs/newPrimeGenerator.js’); // use newly discovered prime for uniqueness
 var asplode = require(’secret/hyperspace.js’); // hard to explain, but very, very effective in-the-moment algorithm 
 var transform = require(’secret/magicVoodooAwesomesauce.js’); // this changes everything
