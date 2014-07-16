3d3abe2afa62b9062f9931913d219126e6e16cc1
Thor Muller <thor@thormuller.com>
2014-07-16 12:37:40 -0700

    Added main script

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reverse Engineering YxYY
+In the spirit of our community of sharing, I though it would worthwhile to open source the code behind YxYY, but it seems that nobody knows where the original code is (apparently, it's a daemon on some hidden subsystem; no associated GitHub repos, etc). So, I thought perhaps we could reverse engineer it. Here's my start with javascript (hey, it's the language of the Web, people!). Also, please note that there are few references to other libraries that may rely on unknown remote APIs (e.g. magicVoodooAwesomesauce.js, mojoEntrancinator.js). I'm sure you guys will have ideas for how to improve on this very, very basic start.
+
+Thanks again, all of you, for making my life immeasurably richer.
---
+++ yxyy.js
@@ -0,0 +1,57 @@
+
+var yaysayers = require(‘data/yaysayers.json’); // all the people
+var modes = [‘make', ‘connect’, ‘hack', ‘swim', ‘dance', ‘think', ‘reflect’, ’sing’, ‘imagine’, ‘love’, ‘celebrate’, ‘thank’]; // for starters
+var seed =  require(‘libs/newPrimeGenerator.js’); // use newly discovered prime for uniqueness
+var asplode = require(’secret/hyperspace.js’); // hard to explain, but very, very effective in-the-moment algorithm 
+var transform = require(’secret/magicVoodooAwesomesauce.js’); // this changes everything
+var version = 001; // skip 000 the first time through (it’s a singularity)
+
+var yxyy = function () {
+     setInterval(function() {
+          yes();
+          var env = realityDistortionField(seed());
+          if (!_.isFinite(yaysayers.awesome.length)) asplode(); 
+          if (_.uniq(yaysayers, function(yaysayer) {  return yaysayer.ideas; }) {
+               asplode();
+          }
+          if (version < 1000) {
+              version++; 
+         } else {
+              version = 000;
+         }
+     }, (52 * 7 * 24* 60 * 60 * 1000);  //  repeat every 52 weeks
+}
+
+
+var yes = function () {
+     for(i=0; i<yaysayers.length; i++) {
+          if (heart.state==‘no’) yaysayers[i].heart.state = ‘yes’;
+          if (mind.state==‘no’) yaysayers[i].mind.state = ‘yes’;
+          console.log(‘yessified’ + yaysers[i].name);
+     }
+     console.log(‘yessification complete’);
+} 
+
+var serendipity = function () {
+      _.shuffle(modes); // mix up the modes for maximum surprise
+      for (i=0;i<modes.length; i++) {
+         var groupSize = yaysayers[Math.floor(Math.random()*yaysayers.length/2)];
+          var groupMind = [];
+          for (x=0;x<groupSize;x++) {
+               groupMind.push(randomYaysayer());
+          }
+          var spark = {mode: mode[i], group: groupMind); 
+         return spark;
+     }
+}
+
+
+var realityDistortionField(seed) {
+     return transform(seed);
+}
+
+var randomYaysayer = function() {
+     return yaysayers[Math.floor(Math.random()*yaysayers.length)];
+}
+
+yxyy(); // begin
